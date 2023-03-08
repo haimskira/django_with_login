7a1f7aad17ae57fa69fdf36785e6ce549a11ea9b
Haim skira <haim1418@gmail.com>
2023-03-09 00:18:23 +0200

    add ugly style

--- base/views.py
+++ base/views.py
@@ -7,17 +7,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import serializers
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
-# @api_view(['GET','POST'])
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
