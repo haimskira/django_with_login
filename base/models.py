89164036f3cef8040b2b347f99a604b6dfe0af72
Haim skira <haim1418@gmail.com>
2023-03-12 19:03:13 +0200

    add user auth & display

--- base/models.py
+++ base/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Student(models.Model):
+    user =models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=50)
     age = models.DecimalField(max_digits=3,decimal_places=0)
     email = models.EmailField(max_length=50,null=True,blank=True)
--- base/views.py
+++ base/views.py
@@ -13,6 +13,11 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'
+    def create(self, validated_data):
+        user = self.context['user']
+        print(user)
+        return Student.objects.create(**validated_data,user=user)
+
 
 @permission_classes([IsAuthenticated])
 class student_Views(APIView):
@@ -22,12 +27,12 @@ class student_Views(APIView):
             my_model = Student.objects.get(id=pk)
             serializer = StudentSerializer(my_model, many=False)
         else:
-            my_model = Student.objects.all()
+            my_model = request.user.student_set.all()
             serializer = StudentSerializer(my_model, many=True)
         return Response(serializer.data)
 
     def post(self, request):  # axios.post
-        serializer = StudentSerializer(data=request.data)
+        serializer = StudentSerializer(data=request.data, context={'user': request.user})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
